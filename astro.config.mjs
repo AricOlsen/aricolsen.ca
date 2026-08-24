41954906e625bec1d43f443e9c49f2062628f24f
AricOlsen <aric.olsen4.0@gmail.com>
2026-08-24 16:40:58 -0400

    Added custom stylings and hero card

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: '[https://aricolsen.ca](https://aricolsen.ca)',
+  site: 'https://aricolsen.ca',
   integrations: [mdx(), sitemap()],
 
   fonts: [
